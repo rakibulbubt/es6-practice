8f050db758d51dab79fa9e0ee8bae7a44f09887c
Rakibul Hasan <rakibul.bcpsc@com>
2022-06-03 01:23:21 +0600

    Add existing file

---
+++ arrow.js
@@ -0,0 +1,27 @@
+// function doubleIt(num) {
+//     return num * 2;
+// }
+
+// const doubleIt = function(num) {
+//     return num * 2
+// }
+
+// const doubleIt = num => num * 2;
+// const add = (x, y) => x + y;
+// const give5 = () => 5
+
+// const result = doubleIt(5);
+// const result2 = add(5, 25);
+// const result3 = give5()
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+
+const bishal = (x, y) => {
+    const sum = (x + y);
+    const diff = (x - y);
+    const rr = diff * sum
+    return rr;
+}
+const result4 = bishal(10, 5);
+console.log(result4);
---
+++ letconst.js
@@ -0,0 +1,28 @@
+// //const = not changeaBLE
+// const wife = "Jannatul Ferdous"
+// console.log(wife);
+
+// const numbers = [12, 14];
+// numbers[2] = 88;
+// numbers.push(56);
+// console.log(numbers);
+// const nayok = { name: "SaKib Khan", phone: "0152483644" };
+// console.log(nayok)
+
+// //now let = changeable but not leakeproof
+// let patientName = "Rahim Chacha";
+// patientName = "Fatema Khala"
+// console.log(patientName);
+
+// let sum = 0;
+// for (let i = 0; i < 10; i++) {
+//     sum = sum + i
+//     console.log(i);
+// }
+
+
+const firstname = "Justin";
+const lastname = "Timber";
+const fullname = firstname + lastname;
+const fullname2 = `${firstname} ${lastname} is a good boy`
+console.log(fullname2);
